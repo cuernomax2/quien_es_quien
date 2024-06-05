9ba24b6ab9270451cbf4ea6933e48c4880209cd3
Cuernomax2 <gonzalezulla.mateomanuel@gmail.com>
2024-06-05 21:02:09 +0200

    add __init__.py + main.pl database + main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from pyswipprolog import Prolog
+import random
+
+prolog = Prolog()
+prolog.consult("src/main.pl")
+
+def character_to_find():
+    characters = list(prolog.query("personaje(Nombre, Caracteristicas)"))
+    character_to_find = random.choice(characters)
+    return character_to_find
+
+
+if __name__ == "__main__":
+    characters = list(prolog.query("personaje(Nombre, Caracteristicas)"))
+    win_condition = character_to_find()
+    print(f"Personaje objetivo --> {win_condition}")
+    question_counter = 0
+
+
